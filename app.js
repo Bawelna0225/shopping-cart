bcb063e81484f45d81c2e88a2834739e33141b15
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-04-02 14:22:52 +0200

    slider

--- app.js
+++ app.js
@@ -229,3 +229,58 @@ sections.forEach(section => {
 
 
 ///////////////////////// Slider ///////////////////////////////////
+const slides = document.querySelectorAll('.slide')
+const nextBtn = document.querySelector('#next')
+const prevBtn = document.querySelector('#prev')
+const auto = false
+const intervalTime = 5000
+let slideInterval
+
+const nextSlide = () => {
+  const currentSlide = document.querySelector('.current')
+  
+  currentSlide.classList.remove('current')
+  
+  if (currentSlide.nextElementSibling) {
+    currentSlide.nextElementSibling.classList.add('current')
+  } else {
+    slides[0].classList.add('current')
+  }
+  
+  setTimeout(() => currentSlide.classList.remove('current'))
+}
+
+const prevSlide = () => {
+  const current = document.querySelector('.current')
+  current.classList.remove('current')
+  if(current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current')
+  } else {
+    slides[slides.length - 1].classList.add('current')
+  }
+  
+  setTimeout(() => current.classList.remove('current'))
+}
+
+
+nextBtn.addEventListener('click', e => {
+  nextSlide() 
+  if(auto) {
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSlide, intervalTime)
+  }
+})
+
+
+prevBtn.addEventListener('click', e => {
+  prevSlide() 
+  if(auto) {
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSlide, intervalTime)
+  }
+})
+
+
+if(auto) {
+  slideInterval = setInterval(nextSlide, intervalTime)
+}
